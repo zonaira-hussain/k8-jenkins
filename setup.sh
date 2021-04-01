d2f6de039ab69dc2ba760a542e673fca967dac0e
zonaira-hussain <78487653+zonaira-hussain@users.noreply.github.com>
2021-04-01 12:18:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-clusterExist=$(eksctl get cluster | grep JenkinsCluster)
-var_length=${#clusterExist}
+echo 'launching cluster'
+eksctl create cluster \
+--name JenkinsCluster \
+--region eu-west-2 \
+--nodegroup-name DemoNodes \
+--nodes 2 \
+--nodes-min 2 \
+--nodes-max 3 \
+--node-type t2.micro \
+--with-oidc \
+--ssh-access \
+--ssh-public-key aws-instance-keypair \
+--managed
 
-echo ${var_length}
-
-if [ 60<1 ]
-	then
-	echo 'launching cluster'
-	eksctl create cluster \
-	--name JenkinsCluster \
-	--region eu-west-2 \
-	--nodegroup-name DemoNodes \
-	--nodes 2 \
-	--nodes-min 2 \
-	--nodes-max 3 \
-	--node-type t2.micro \
-	--with-oidc \
-	--ssh-access \
-	--ssh-public-key TerraformPair \
-	--managed
-fi
